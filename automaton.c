a09932bbdac84312074c36b006178a032817471a
Jun Jie <limjunjie97@gmail.com>
2024-04-25 13:38:26 +1000

    Frequency Prefix Automaton

--- automaton.c
+++ automaton.c
@@ -1,4 +1,4 @@
-/* 
+/*
 Program to generate text based on the context provided by input prompts.
 Skeleton program written by Artem Polyvyanyy, http://polyvyanyy.com/,
 September 2023, with the intention that it be modified by students
@@ -33,9 +33,6 @@ available via such a service, may be regarded as Student General Misconduct
 others to commit Academic Misconduct). I understand that an allegation of
 Student General Misconduct may arise regardless of whether or not I personally
 make use of such solutions or sought benefit from such actions.
-
-Signed by: LIM JUN JIE (STUDENT ID: 1228915)
-Dated: October 11th, 2023
 */
 
 #include <stdio.h>
@@ -44,15 +41,15 @@ Dated: October 11th, 2023
 #include <string.h>
 
 /* #DEFINE'S -----------------------------------------------------------------*/
-#define SDELIM "==STAGE %d============================\n"   // stage delimiter
-#define MDELIM "-------------------------------------\n"    // delimiter of -'s
-#define THEEND "==THE END============================\n"    // end message
-#define NOSFMT "Number of statements: %d\n"                 // no. of statements
-#define NOCFMT "Number of characters: %d\n"                 // no. of chars
-#define NPSFMT "Number of states: %d\n"                     // no. of states
-#define TFQFMT "Total frequency: %d\n"                      // total frequency
+#define SDELIM "==STAGE %d============================\n" // stage delimiter
+#define MDELIM "-------------------------------------\n"  // delimiter of -'s
+#define THEEND "==THE END============================\n"  // end message
+#define NOSFMT "Number of statements: %d\n"               // no. of statements
+#define NOCFMT "Number of characters: %d\n"               // no. of chars
+#define NPSFMT "Number of states: %d\n"                   // no. of states
+#define TFQFMT "Total frequency: %d\n"                    // total frequency
 
-#define CRTRNC '\r'                             // carriage return character
+#define CRTRNC '\r' // carriage return character
 #define BLANK ' '
 #define STAGE_ZERO 0
 #define STAGE_ONE 1
@@ -62,36 +59,40 @@ Dated: October 11th, 2023
 #define STAGE_TWO 2
 
 /* TYPE DEFINITIONS ----------------------------------------------------------*/
-typedef struct state state_t;   // a state in an automaton
-typedef struct node  node_t;    // a node in a linked list
+typedef struct state state_t; // a state in an automaton
+typedef struct node node_t;   // a node in a linked list
 
-struct node {                   // a node in a linked list of transitions has
-    char*           str;        // ... a transition string
-    state_t*        state;      // ... the state reached via the string, and
-    node_t*         next;       // ... a link to the next node in the list.
+struct node
+{                   // a node in a linked list of transitions has
+    char *str;      // ... a transition string
+    state_t *state; // ... the state reached via the string, and
+    node_t *next;   // ... a link to the next node in the list.
 };
 
-typedef struct {                // a linked list consists of
-    node_t*         head;       // ... a pointer to the first node and
-    node_t*         tail;       // ... a pointer to the last node in the list.
+typedef struct
+{                 // a linked list consists of
+    node_t *head; // ... a pointer to the first node and
+    node_t *tail; // ... a pointer to the last node in the list.
 } list_t;
 
-struct state {                  // a state in an automaton is characterized by
-    unsigned int    id;         // ... an identifier,
-    unsigned int    freq;       // ... frequency of traversal,
-    int             visited;    // ... visited status flag, and
-    list_t*         outputs;    // ... a list of output states.
+struct state
+{                      // a state in an automaton is characterized by
+    unsigned int id;   // ... an identifier,
+    unsigned int freq; // ... frequency of traversal,
+    int visited;       // ... visited status flag, and
+    list_t *outputs;   // ... a list of output states.
 };
 
-typedef struct {                // an automaton consists of
-    state_t*        ini;        // ... the initial state, and
-    unsigned int    nid;        // ... the identifier of the next new state.
-    unsigned int    statements;
-    unsigned int    characters;
+typedef struct
+{                     // an automaton consists of
+    state_t *ini;     // ... the initial state, and
+    unsigned int nid; // ... the identifier of the next new state.
+    unsigned int statements;
+    unsigned int characters;
 } automaton_t;
 int count = 0;
 /* USEFUL FUNCTIONS ----------------------------------------------------------*/
-int mygetchar(int n);            // getchar() that skips carriage returns
+int mygetchar(int n); // getchar() that skips carriage returns
 
 automaton_t *make_EmptyAutomaton(automaton_t *automaton);
 
@@ -108,7 +109,8 @@ void stage1(automaton_t *automaton);
 void stage2(automaton_t *automaton);
 
 /* WHERE IT ALL HAPPENS ------------------------------------------------------*/
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     automaton_t automaton = *make_EmptyAutomaton(&automaton);
     make_FirstRun(&automaton);
     stage0(&automaton);
@@ -117,25 +119,27 @@ int main(int argc, char *argv[]) {
     return EXIT_SUCCESS;
 }
 
-void
-stage2(automaton_t *automaton) {
+void stage2(automaton_t *automaton)
+{
     char curr_c = mygetchar(count++), past_c = BLANK;
     int steps = 0, tens = 1;
-    while(curr_c >= 48 && curr_c <= 57) {
+    while (curr_c >= 48 && curr_c <= 57)
+    {
         steps = (curr_c - ASCII_NUM) * tens;
         tens *= 10;
         past_c = curr_c;
         curr_c = mygetchar(count++);
     }
     printf(SDELIM, STAGE_TWO);
-    
-    while(steps > 0) {
+
+    while (steps > 0)
+    {
         break;
     }
 }
 
-void
-stage1(automaton_t *automaton) {
+void stage1(automaton_t *automaton)
+{
     node_t *curr_ptr = automaton->ini->outputs->head;
     char curr_c = mygetchar(count++), past_c = BLANK;
     node_t *most_freq = curr_ptr;
@@ -143,30 +147,43 @@ stage1(automaton_t *automaton) {
     char most_freqChar = '\0';
     int digit_count = MAX_DIGITS;
     printf(SDELIM, STAGE_ONE);
-    while(1) {
+    while (1)
+    {
         // Ends stage 1 when empty line is detected
-        if(curr_c == '\n' && past_c == '\n') {
+        if (curr_c == '\n' && past_c == '\n')
+        {
             break;
         }
         digit_count = MAX_DIGITS;
         // Traversing the automaton until new line character detected
-        while(curr_c != '\n') {
-            if(digit_count > 0) {
+        while (curr_c != '\n')
+        {
+            if (digit_count > 0)
+            {
                 printf("%c", curr_c);
                 digit_count--;
             }
-            while(1) {
-                if(*(curr_ptr->str) == EMPTY) {
+            while (1)
+            {
+                if (*(curr_ptr->str) == EMPTY)
+                {
                     break;
-                } else if (*(curr_ptr->str) == curr_c) {
+                }
+                else if (*(curr_ptr->str) == curr_c)
+                {
                     break;
-                } else {
+                }
+                else
+                {
                     curr_ptr = curr_ptr->next;
                 }
             }
-            if(*(curr_ptr->str) == EMPTY) {
+            if (*(curr_ptr->str) == EMPTY)
+            {
                 break;
-            } else if (*(curr_ptr->str) == curr_c) {
+            }
+            else if (*(curr_ptr->str) == curr_c)
+            {
                 curr_ptr = curr_ptr->state->outputs->head;
                 past_c = curr_c;
                 curr_c = mygetchar(count++);
@@ -174,44 +191,58 @@ stage1(automaton_t *automaton) {
             }
         }
         // Prints the ellipses
-        if(digit_count > 0) {
+        if (digit_count > 0)
+        {
             printf(".");
             digit_count--;
         }
-        if(digit_count > 0) {
+        if (digit_count > 0)
+        {
             printf(".");
             digit_count--;
         }
-        if(digit_count > 0) {
+        if (digit_count > 0)
+        {
             printf(".");
             digit_count--;
         }
         // Prints most frequent character until leaf node
-        while(1) {
-            if(*(curr_ptr->str) == EMPTY) {
+        while (1)
+        {
+            if (*(curr_ptr->str) == EMPTY)
+            {
                 break;
             }
-            while(*(curr_ptr->str) != EMPTY) {
-                if(curr_ptr->state->freq == most_freqNum) {
-                    if(most_freqChar == EMPTY) {
-                        most_freqChar = *(curr_ptr->str);
-                        most_freqNum = curr_ptr->state->freq;
-                        most_freq = curr_ptr;
-                    } else if(*(curr_ptr->str) == most_freqChar) {
-                        break;
-                    } else if(*(curr_ptr->str) > most_freqChar) {
+            while (*(curr_ptr->str) != EMPTY)
+            {
+                if (curr_ptr->state->freq == most_freqNum)
+                {
+                    if (most_freqChar == EMPTY)
+                    {
                         most_freqChar = *(curr_ptr->str);
                         most_freqNum = curr_ptr->state->freq;
                         most_freq = curr_ptr;
                     }
-                } else if(curr_ptr->state->freq > most_freqNum) {
+                    else if (*(curr_ptr->str) == most_freqChar)
+                    {
+                        break;
+                    }
+                    else if (*(curr_ptr->str) > most_freqChar)
+                    {
+                        most_freqChar = *(curr_ptr->str);
+                        most_freqNum = curr_ptr->state->freq;
+                        most_freq = curr_ptr;
+                    }
+                }
+                else if (curr_ptr->state->freq > most_freqNum)
+                {
                     most_freqNum = curr_ptr->state->freq;
                     most_freq = curr_ptr;
                     most_freqChar = *(curr_ptr->str);
-                } 
+                }
                 curr_ptr = curr_ptr->next;
             }
-            
+
             printf("%c", most_freqChar);
             digit_count--;
             curr_ptr = most_freq;
@@ -223,10 +254,14 @@ stage1(automaton_t *automaton) {
         past_c = curr_c;
         curr_c = mygetchar(count++);
         curr_ptr = automaton->ini->outputs->head;
-        while(1) {
-            if(past_c == '\n') {
+        while (1)
+        {
+            if (past_c == '\n')
+            {
                 break;
-            } else {
+            }
+            else
+            {
                 past_c = curr_c;
                 curr_c = mygetchar(count++);
             }
@@ -234,32 +269,42 @@ stage1(automaton_t *automaton) {
     }
 }
 
-void
-stage0(automaton_t *automaton) {
-    int NewStatementFlag = 1;    // when start of new statement
+void stage0(automaton_t *automaton)
+{
+    int NewStatementFlag = 1; // when start of new statement
     char past_c = BLANK, curr_c = mygetchar(count++);
     node_t *curr_ptr = automaton->ini->outputs->head;
 
-    while(1) {
+    while (1)
+    {
         // Ends stage 0 when empty line is detected
-        if(curr_c == '\n' && past_c == '\n') {
+        if (curr_c == '\n' && past_c == '\n')
+        {
             break;
         }
         // First character of each new statement
-        if(NewStatementFlag == 1) {  
+        if (NewStatementFlag == 1)
+        {
             // New statement detected
             automaton->statements = (int)(automaton->statements) + 1;
             curr_ptr = automaton->ini->outputs->head;
-            while(1) {
-                if(*(curr_ptr->str) == EMPTY) {
+            while (1)
+            {
+                if (*(curr_ptr->str) == EMPTY)
+                {
                     break;
-                } else if (*(curr_ptr->str) == curr_c) {
+                }
+                else if (*(curr_ptr->str) == curr_c)
+                {
                     break;
-                } else {
+                }
+                else
+                {
                     curr_ptr = curr_ptr->next;
                 }
             }
-            if(*(curr_ptr->str) == EMPTY) {
+            if (*(curr_ptr->str) == EMPTY)
+            {
                 make_node(automaton, curr_ptr, curr_c);
                 curr_ptr = curr_ptr->state->outputs->head;
                 past_c = curr_c;
@@ -267,7 +312,9 @@ stage0(automaton_t *automaton) {
                 automaton->characters = (int)(automaton->characters) + 1;
                 NewStatementFlag = 0;
                 continue;
-            } else {
+            }
+            else
+            {
                 curr_ptr->state->freq = (int)(curr_ptr->state->freq) + 1;
                 curr_ptr = curr_ptr->state->outputs->head;
                 past_c = curr_c;
@@ -278,43 +325,59 @@ stage0(automaton_t *automaton) {
             }
         }
         // Subsequent characters of each statement
-        while(NewStatementFlag == 0) {
-            if(curr_c == '\n') {
+        while (NewStatementFlag == 0)
+        {
+            if (curr_c == '\n')
+            {
                 NewStatementFlag = 1;
                 break;
             }
-            if(*(curr_ptr->str) == EMPTY) {
+            if (*(curr_ptr->str) == EMPTY)
+            {
                 make_node(automaton, curr_ptr, curr_c);
                 curr_ptr = curr_ptr->state->outputs->head;
                 past_c = curr_c;
                 curr_c = mygetchar(count++);
                 automaton->characters = (int)(automaton->characters) + 1;
                 continue;
-            } else if(*(curr_ptr->str) == curr_c) {
+            }
+            else if (*(curr_ptr->str) == curr_c)
+            {
                 curr_ptr->state->freq = (int)(curr_ptr->state->freq) + 1;
                 curr_ptr = curr_ptr->state->outputs->head;
                 past_c = curr_c;
                 curr_c = mygetchar(count++);
                 automaton->characters = (int)(automaton->characters) + 1;
                 continue;
-            } else {
-                while(1) {
-                    if(*(curr_ptr->str) == EMPTY) {
+            }
+            else
+            {
+                while (1)
+                {
+                    if (*(curr_ptr->str) == EMPTY)
+                    {
                         break;
-                    } else if (*(curr_ptr->str) == curr_c) {
+                    }
+                    else if (*(curr_ptr->str) == curr_c)
+                    {
                         break;
-                    } else {
+                    }
+                    else
+                    {
                         curr_ptr = curr_ptr->next;
                     }
                 }
-                if(*(curr_ptr->str) == EMPTY) {
+                if (*(curr_ptr->str) == EMPTY)
+                {
                     make_node(automaton, curr_ptr, curr_c);
                     curr_ptr = curr_ptr->state->outputs->head;
                     past_c = curr_c;
                     curr_c = mygetchar(count++);
                     automaton->characters = (int)(automaton->characters) + 1;
                     continue;
-                } else if (*(curr_ptr->str) == curr_c) {
+                }
+                else if (*(curr_ptr->str) == curr_c)
+                {
                     curr_ptr->state->freq = (int)(curr_ptr->state->freq) + 1;
                     curr_ptr = curr_ptr->state->outputs->head;
                     past_c = curr_c;
@@ -333,19 +396,20 @@ stage0(automaton_t *automaton) {
     printf(NPSFMT, automaton->nid);
 }
 
-void
-make_FirstRun(automaton_t *automaton) {
+void make_FirstRun(automaton_t *automaton)
+{
     char *curr_c = (char *)malloc(sizeof(char));
     *(curr_c) = mygetchar(count++);
     node_t *curr_ptr = automaton->ini->outputs->head;
-    
-    //First statement recorded into automaton
+
+    // First statement recorded into automaton
     automaton->statements = (int)(automaton->statements) + 1;
-    
-    while(*(curr_c) != '\n') {
+
+    while (*(curr_c) != '\n')
+    {
         // Each character of the first statement registered
         automaton->characters = (int)(automaton->characters) + 1;
-        
+
         make_node(automaton, curr_ptr, curr_c);
         *(curr_c) = mygetchar(count++);
         curr_ptr = curr_ptr->state->outputs->head;
@@ -353,14 +417,14 @@ make_FirstRun(automaton_t *automaton) {
     return;
 }
 
-void
-make_node(automaton_t *automaton, node_t *node, char *c) {
+void make_node(automaton_t *automaton, node_t *node, char *c)
+{
     strcpy(node->str, c);
-    
+
     node->state = (state_t *)malloc(sizeof(state_t));
     assert(node->state);
     make_state(automaton, node->state);
-    
+
     node->next = (node_t *)malloc(sizeof(node_t));
     assert(node->next);
     node->next->str = (char *)malloc(sizeof(char));
@@ -369,14 +433,14 @@ make_node(automaton_t *automaton, node_t *node, char *c) {
     return;
 }
 
-void
-make_state(automaton_t *automaton, state_t *state) {
+void make_state(automaton_t *automaton, state_t *state)
+{
     automaton->nid = (int)(automaton->nid) + 1;
-    
+
     state->id = automaton->nid;
     state->freq = 0;
     state->visited = 0;
-    
+
     state->outputs = (list_t *)malloc(sizeof(list_t));
     assert(state->outputs);
     state->outputs->head = (node_t *)malloc(sizeof(node_t));
@@ -387,8 +451,8 @@ make_state(automaton_t *automaton, state_t *state) {
     return;
 }
 
-automaton_t
-*make_EmptyAutomaton(automaton_t *automaton) {
+automaton_t *make_EmptyAutomaton(automaton_t *automaton)
+{
     automaton = (automaton_t *)malloc(sizeof(automaton_t));
     assert(automaton);
     automaton->ini = (state_t *)malloc(sizeof(state_t));
@@ -408,20 +472,21 @@ automaton_t
     automaton->nid = 1;
     automaton->statements = 0;
     automaton->characters = 0;
-    
+
     return automaton;
 }
 
-int mygetchar(int n) {
+int mygetchar(int n)
+{
     char c;
     char t[] = "Hey#Prof\nHi#Sir\nHi#Sir\nHi#Prof\nHello\nHey\nHi#there\nHey#you\n\n";
-    if(count >= 60) {
+    if (count >= 60)
+    {
         c = '\n';
         return c;
     }
     c = *(t + n);
     return c;
 }
-
 
 // algorithms are fun
